c5d7cb9486e62278f8e34c150b29fa403aaf8381
Arnab Das <ad964976@gmail.com>
2024-02-14 23:51:48 +0530

    Hello print

---
+++ Loops_Question1.java
@@ -0,0 +1,27 @@
+/*
+ 
+How many times 'Hello' is printed?
+
+public class Solution{
+    public static void main(String[]args){
+        for(int i= 0 ; i < 5 ; i++){
+            System.out.println("Hello");
+            i+=2;
+        }
+    }
+}
+
+ */
+
+ import java.util.*;
+public class Loops_Question1 {
+    public static void main (String args[]){
+        Scanner sc = new Scanner (System.in);
+
+        for(int i= 0 ; i < 5 ; i++){
+            System.out.println("Hello");
+            i+=2;
+        }
+
+    }
+}
