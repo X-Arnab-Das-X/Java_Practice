d04e12a0a45c77877884a04218ed8271bdbfb461
Arnab Das <ad964976@gmail.com>
2024-02-17 20:31:11 +0530

    Hello World (For Loop)

---
+++ ForLoop_PrintHelloWorld100.java
@@ -0,0 +1,10 @@
+import java.util.*;
+public class ForLoop_PrintHelloWorld100 {
+    public static void main (String args[]){
+        Scanner sc = new Scanner (System.in);
+
+        for ( int i = 1 ; i <= 100 ; i++){
+            System.out.print("Hello World\n");
+        }
+    }
+}
