00c581d5a4142a64a423f9ba179f1b1d2d782767
Arnab Das <ad964976@gmail.com>
2024-02-18 22:19:00 +0530

    Half Pyramid Pattern

---
+++ HalfPyramidPattern_part1.java
@@ -0,0 +1,31 @@
+/*
+ 
+
+    1
+    1 2
+    1 2 3
+    1 2 3 4
+    1 2 3 4 5
+
+
+ */
+
+ import java.util.*;
+ public class HalfPyramidPattern_part1 {
+    public static void main (String args[]){
+        Scanner sc = new Scanner (System.in);
+
+        System.out.print("Enter row number\n");
+
+        int n = sc.nextInt();
+
+        for (int i = 1 ; i <= n ; i++){
+            for (int j = 1 ; j <= i ; j++){
+                System.out.print( j + " " );
+            }
+            System.out.print("\n");
+        }
+
+
+    }
+ }
