b61be33a5f6f432ad6a4775fd31237f714db3d60
Arnab Das <ad964976@gmail.com>
2024-02-18 21:46:17 +0530

    Answer

---
+++ Operators_Question_5.java
@@ -0,0 +1,32 @@
+/*
+ 
+
+What will be the output of the following programs :
+
+public class Test {
+    public static void main(String[] args){
+        int x = 10, y = 5;
+        int exp1 = (y * (x / y + x / y));
+        int exp2 = (y * x / y + y * x / y);
+        System.out.println(exp1);
+        System.out.println(exp2);
+    }
+}
+
+
+ */
+
+
+ import java.util.*;
+
+ public class Operators_Question_5 {
+    public static void main (String args[]){
+        Scanner sc = new Scanner (System.in);
+
+        int x = 10, y = 5;
+        int exp1 = (y * (x / y + x / y));
+        int exp2 = (y * x / y + y * x / y);
+        System.out.println(exp1);
+        System.out.println(exp2);
+    }
+ }
