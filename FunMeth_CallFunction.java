f8d8736b08d03b4279126df52eda35d88c5f1643
Arnab Das <ad964976@gmail.com>
2024-02-20 22:42:46 +0530

    Call Function

---
+++ FunMeth_CallFunction.java
@@ -0,0 +1,15 @@
+public class FunMeth_CallFunction {
+
+    public static void printHelloWorld (){
+
+        System.out.println("Hello World");
+        System.out.println("Hello World");
+        System.out.println("Hello World");
+
+    }
+    public static void main (String args[]){
+
+        printHelloWorld();
+
+    }
+}
